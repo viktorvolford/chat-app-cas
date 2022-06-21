278060abc7cbe77a194101d04f280987ff936879
Viktor Volford <randvstudio@gmail.com>
2022-06-21 14:01:43 +0200

    Room list cards design modified

--- src/app/pages/main/room-list/room-list.component.ts
+++ src/app/pages/main/room-list/room-list.component.ts
@@ -6,6 +6,8 @@ import { Router } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog'
 import { ProtectedDialogComponent } from './protected-dialog/protected-dialog.component';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { UserService } from '../../../shared/services/user.service';
+import { User } from '../../../shared/models/User';
 
 @Component({
   selector: 'app-room-list',
@@ -17,20 +19,26 @@ export class RoomListComponent implements OnInit, OnChanges, OnDestroy {
   @Input() chosenVisibility: string | undefined;
 
   rooms: Array<Room> = new Array<Room>();
+  users: Array<User> = new Array<User>();
 
   roomsLoadingSubscription?: Subscription;
+  usersLoadingSubscription?: Subscription;
   dialogSubscription?: Subscription;
 
   constructor(
     private roomService: RoomService,
     private router: Router,
     private dialog: MatDialog,
-    private snackBar: MatSnackBar
+    private snackBar: MatSnackBar,
+    private userService: UserService
     ) { }
 
   ngOnChanges(): void {
     this.roomsLoadingSubscription = this.roomService.getRooms(this.chosenVisibility as string).subscribe((data: Array<Room>) => {
       this.rooms = data;
+    });
+    this.usersLoadingSubscription = this.userService.getAll().subscribe((data: Array<User>) => {
+      this.users = data;
     });
   }
 
@@ -39,6 +47,7 @@ export class RoomListComponent implements OnInit, OnChanges, OnDestroy {
   ngOnDestroy(): void {
     this.roomsLoadingSubscription?.unsubscribe();
     this.dialogSubscription?.unsubscribe();
+    this.usersLoadingSubscription?.unsubscribe();
   }
 
   openChatroom(id: string){
@@ -55,6 +64,10 @@ export class RoomListComponent implements OnInit, OnChanges, OnDestroy {
         this.snackBar.open('The entered password was incorrect!', 'OK', {duration: 2000});
       }
     });
+  }
+
+  getUsername(id: string){
+    return this.users?.find(user => user.id === id)?.username;
   }
 
 }
