6de71ddbadb09486b95e13a659d380254f6e569d
Viktor Volford <randvstudio@gmail.com>
2022-07-11 09:03:25 +0200

    MessageService reorganized

--- src/app/shared/services/message.service.ts
+++ src/app/shared/services/message.service.ts
@@ -5,28 +5,20 @@ import { combineLatest, map, of, ReplaySubject, share, switchMap, take } from 'r
 import { FormGroup } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { EditDialogComponent } from '../../pages/main/conversation/edit-dialog/edit-dialog.component';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { TranslateService } from '@ngx-translate/core';
+import { ToastService } from './toast.service';
 
 @Injectable()
 export class MessageService {
 
-  collectionName = 'Messages';
+  private collectionName = 'Messages';
 
   constructor(
-    private afs: AngularFirestore,
-    private dialog: MatDialog,
-    private snackBar: MatSnackBar,
-    private translate: TranslateService
+    private readonly afs: AngularFirestore,
+    private readonly dialog: MatDialog,
+    private readonly toast: ToastService
     ) { }
 
-  create(message: Message) {
-    return this.afs.collection<Message>(this.collectionName).add(message).then(res => {
-      this.afs.collection<Message>(this.collectionName).doc(res.id).update({id: res.id});
-    });
-  }
-
-  getMessagesforRoom(id: string) {
+  public getMessagesforRoom(id: string) {
     return this.afs.collection<Message>(this.collectionName, ref => ref
       .where('type', '==', 'room')
       .where('target_id', '==', id)
@@ -39,7 +31,7 @@ export class MessageService {
       );
   }
 
-  getPersonalMessages(firstId: string, secondId: string) {
+  public getPersonalMessages(firstId: string, secondId: string) {
      const first = this.afs.collection<Message>(this.collectionName, ref => ref
         .where('type', '==', 'personal')
         .where('sender_id', '==', firstId)
@@ -70,15 +62,13 @@ export class MessageService {
       );
   }
 
-  update(message: Message) {
-    return this.afs.collection<Message>(this.collectionName).doc(message.id).set(message);
+  public delete(id: string) : void {
+    this.afs.collection<Message>(this.collectionName).doc(id).delete().then(_ => {
+      this.toast.createSnackBar('CONVERSATION.DELETED', 'COMMON.OK');
+    });
   }
 
-  delete(id: string) {
-    return this.afs.collection<Message>(this.collectionName).doc(id).delete();
-  }
-
-  sendMessage(form: FormGroup){
+  public sendMessage(form: FormGroup){
     if (form.get('content')?.value) {
       form.get('date')?.setValue(new Date().getTime());
 
@@ -90,24 +80,28 @@ export class MessageService {
     }
   }
 
-  openEditDialog(message: Message){
+  public openEditDialog(message: Message){
     const dialogRef = this.dialog.open(EditDialogComponent, {data: {message}});
 
     dialogRef.afterClosed().pipe(take(1)).subscribe(result => {
       if(result && result !== message.content){
         message.content = result; 
         this.update(message).then(() => {
-          this.snackBar.open(
-            this.translate.instant('CONVERSATION.MODIFIED'), 
-            this.translate.instant('COMMON.OK'), 
-            {duration: 2000});
+          this.toast.createSnackBar('CONVERSATION.MODIFIED', 'COMMON.OK');
         });
       } else if(result === ''){
-        this.snackBar.open(
-          this.translate.instant('CONVERSATION.EMPTY'), 
-          this.translate.instant('COMMON.OK'), 
-          {duration: 2000});
+        this.toast.createSnackBar('CONVERSATION.EMPTY', 'COMMON.OK');
       }
     });
+  }
+
+  private create(message: Message) {
+    return this.afs.collection<Message>(this.collectionName).add(message).then(res => {
+      this.afs.collection<Message>(this.collectionName).doc(res.id).update({id: res.id});
+    });
+  }
+
+  private update(message: Message) {
+    return this.afs.collection<Message>(this.collectionName).doc(message.id).set(message);
   }
 }
